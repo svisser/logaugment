285bbfd392eec279c18ca0cd1d45e13ea56147ac
Simeon Visser <simeon87@gmail.com>
2014-08-29 23:38:47 +0100

    Moved Filter to be outside function

--- logaugment/__init__.py
+++ logaugment/__init__.py
@@ -1,18 +1,19 @@
 import logging
 
 
+class AugmentAddFilter(logging.Filter):
+        
+    def __init__(self, name='', args=None):
+        super(AddFilter, self).__init__(name)
+        self.args = args
+    
+    def filter(self, record):
+        if self.args is not None:
+            for key, value in self.args.items():
+                setattr(record, key, value)
+        return True
+
+
 def add(logger, args):
-    
-    class AddFilter(logging.Filter):
-        
-        def __init__(self, name='', args=None):
-            super(AddFilter, self).__init__(name)
-            self.args = args
-        
-        def filter(self, record):
-            if self.args is not None:
-                for key, value in self.args.items():
-                    setattr(record, key, value)
-            return True
-    
-    logger.addFilter(AddFilter(name='logaugment.AddFilter', args=args))
+    new_filter = AugmentAddFilter(name='logaugment.AugmentAddFilter', args=args)
+    logger.addFilter(new_filter)
